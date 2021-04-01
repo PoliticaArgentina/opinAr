c491bc0e2a26d8a78f7b559251dfbf18b0393a8f
TuQmano-Lenovo <juanpabloruiznicolini@gmail.com>
2021-04-01 00:29:46 -0300

    get_icg_raw() first version

---
+++ R/get_icg_raw.R
@@ -0,0 +1,44 @@
+#' Descarga base de microdatos del ICG
+#'   (\emph{(Universidad Torcuato Di Tella)})
+#'@description
+#'
+#'Función que ...
+#'
+#'@return Devuelve un objeto "tbl_df" "tbl" "data.frame" con los microdatos del ICG....
+#'
+#'@details Respecto el origen de los datos se puede consultar la documentación en
+#' \href{https://www.utdt.edu/ver_contenido.php?id_contenido=1351&id_item_menu=2970}{\code{https://www.utdt.edu/ver_contenido.php?id_contenido=1351&id_item_menu=2970}}.
+#'
+#'@export
+
+
+get_icg_raw <- function(){
+
+
+  # DATA URL
+  url = "https://www.utdt.edu/ver_contenido.php?id_contenido=17876&id_item_menu=28756"
+
+  # HTML ELEMENT PATH
+  xpath = "/html/body/main/section[2]/div/div/div/div[2]/div/span[2]/strong/a"
+
+  # Set default value for try()
+  default <- NULL
+  df <- base::suppressWarnings(base::try(default <- rvest::read_html(url) %>%
+                                           rvest::html_elements(xpath = xpath) %>%
+                                           rvest::html_attr('href') %>%
+                                           haven:: read_dta() %>%
+                                           janitor::clean_names(),
+                                         silent = TRUE))
+  if(is.null(default)){
+    df <- base::suppressWarnings(base::try(default <- haven::read_dta(file = "https://github.com/politicaargentina/data_warehouse/raw/master/opinAr/data_raw/icg.dta") %>%
+      janitor::clean_names(),
+    silent = TRUE))
+    if(is.null(default)){
+      df <- base::message("Fail to download data. Source is not available // La fuente de datos no esta disponible")
+    } else {
+      df <- df
+    }
+  } else {
+    df
+  }
+}

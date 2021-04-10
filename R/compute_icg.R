750efefcf038c26bae078c60613873be3cbe3ea0
chig4 <camihiga@hotmail.com.ar>
2021-04-10 17:26:31 -0300

    function compute_icg

---
+++ R/compute_icg.R
@@ -0,0 +1,28 @@
+
+compute_icg <- function(data = df, wave = NULL, group = c(NULL, sexo, edad, zona, educacion, sit_ec)){
+ group <- enquo(group)
+
+  if (is.null(wave)){
+
+    df %>%
+      dplyr::mutate(dplyr::across(c(icg, eval_gob_rec, benef_gob_rec, adm_gp_rec, cor_gob_rec, resol_prob_rec),
+                           .fns = list(weighted = ~(.*ponderacion_utdt)),
+                           .names = "{col}_weighted")) %>%
+      dplyr::group_by(ola) %>%
+      dplyr::summarise(dplyr::across(c(icg_weighted, eval_gob_rec_weighted, benef_gob_rec_weighted, adm_gp_rec_weighted, cor_gob_rec_weighted, resol_prob_rec_weighted),
+                              .fns = list(weighted = ~(round(sum(.)/sum(ponderacion_utdt), digits = 2))))) #promedio de ICG
+
+  }
+  else {
+    df %>%
+      dplyr::filter(ola %in% wave) %>%
+      dplyr::mutate(dplyr::across(c(icg, eval_gob_rec, benef_gob_rec, adm_gp_rec, cor_gob_rec, resol_prob_rec),
+                                  .fns = list(weighted = ~(.*ponderacion_utdt)),
+                                  .names = "{col}_weighted")) %>%
+      dplyr::group_by(ola, !!group) %>%
+      dplyr::summarise(dplyr::across(c(icg_weighted, eval_gob_rec_weighted, benef_gob_rec_weighted, adm_gp_rec_weighted, cor_gob_rec_weighted, resol_prob_rec_weighted),
+                                     .fns = list(weighted = ~(round(sum(.)/sum(ponderacion_utdt), digits = 2))))) #promedio de ICG
+  }
+}
+
+

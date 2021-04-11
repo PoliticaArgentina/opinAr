4582400ddaac9eaa47d5c3196db200e8e0baf50d
chig4 <camihiga@hotmail.com.ar>
2021-04-11 14:13:22 -0300

    v3 compute_icg function

--- R/compute_icg.R
+++ R/compute_icg.R
@@ -1,16 +1,18 @@
 
 compute_icg <- function(data = df, wave = NULL, segment_by = NULL){
- segment_by <-dplyr::enquo(segment_by)
+
+   segment_by <-dplyr::enquo(segment_by)
 
   if (is.null(wave)){
 
     df %>%
       dplyr::mutate(dplyr::across(c(icg, eval_gob_rec, benef_gob_rec, adm_gp_rec, cor_gob_rec, resol_prob_rec),
-                           .fns = list(weighted = ~(.*ponderacion_utdt)),
-                           .names = "{col}_weighted")) %>%
-      dplyr::group_by(ola, !!segment_by) %>%
+                                  .fns = list(weighted = ~(.*ponderacion_utdt)),
+                                  .names = "{col}_weighted")) %>%
+      dplyr::group_by(ola, !!segment_by) %>% #para que se desplegue por cualquiera de las variables. Ej. nivel educativo
       dplyr::summarise(dplyr::across(c(icg_weighted, eval_gob_rec_weighted, benef_gob_rec_weighted, adm_gp_rec_weighted, cor_gob_rec_weighted, resol_prob_rec_weighted),
-                              .fns = list(weighted = ~(round(sum(.)/sum(ponderacion_utdt), digits = 2))))) #promedio de ICG
+                                     .fns = list(weighted = ~(base::round(base::sum(.)/base::sum(ponderacion_utdt), digits = 2))))) %>%  #promedio de ICG
+      purrr::set_names(~stringr::str_replace(.,"_weighted_1", ""))#cambio los nombres de las variables
 
   }
   else {
@@ -19,9 +21,11 @@ compute_icg <- function(data = df, wave = NULL, segment_by = NULL){
       dplyr::mutate(dplyr::across(c(icg, eval_gob_rec, benef_gob_rec, adm_gp_rec, cor_gob_rec, resol_prob_rec),
                                   .fns = list(weighted = ~(.*ponderacion_utdt)),
                                   .names = "{col}_weighted")) %>%
-      dplyr::group_by(ola, !!segment_by) %>%
+      dplyr::group_by(ola, !!segment_by) %>% #para que se desplegue por cualquiera de las variables. Ej. nivel educativo
       dplyr::summarise(dplyr::across(c(icg_weighted, eval_gob_rec_weighted, benef_gob_rec_weighted, adm_gp_rec_weighted, cor_gob_rec_weighted, resol_prob_rec_weighted),
-                                     .fns = list(weighted = ~(round(sum(.)/sum(ponderacion_utdt), digits = 2))))) #promedio de ICG
+                                     .fns = list(weighted = ~(base::round(base::sum(.)/base::sum(ponderacion_utdt), digits = 2))))) %>%  #promedio de ICG
+      purrr::set_names(~stringr::str_replace(.,"_weighted_1", ""))#cambio los nombres de las variables
+
   }
 }
 
